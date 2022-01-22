416e827d5538a6bebe6be0b2f51c8cd7d1bddb6d
Jiří Hlaváček <jirka@jhlavacek.cz>
2022-01-22 15:53:34 +0100

    feat: přesunuto na github.com

---
+++ Snowman/Form1.Designer.vb
@@ -0,0 +1,221 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Formulář přepisuje metodu Dispose, aby vyčistil seznam součástí.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Vyžadováno Návrhářem Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'POZNÁMKA: Následující procedura je vyžadována Návrhářem Windows Form
+    'Může být upraveno pomocí Návrháře Windows Form.  
+    'Neupravovat pomocí editoru kódu
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.pboxPlatno = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.tbarSnow = New System.Windows.Forms.TrackBar()
+        Me.tbarSize = New System.Windows.Forms.TrackBar()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.pboxSnowColor = New System.Windows.Forms.PictureBox()
+        Me.pboxSkyColor = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.tbarWind = New System.Windows.Forms.TrackBar()
+        Me.tbarPosition = New System.Windows.Forms.TrackBar()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.tmrTime = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.pboxPlatno, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        CType(Me.tbarSnow, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.tbarSize, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.pboxSnowColor, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pboxSkyColor, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        CType(Me.tbarWind, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.tbarPosition, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pboxPlatno
+        '
+        Me.pboxPlatno.BackColor = System.Drawing.Color.White
+        Me.pboxPlatno.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pboxPlatno.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pboxPlatno.Location = New System.Drawing.Point(107, 0)
+        Me.pboxPlatno.Name = "pboxPlatno"
+        Me.pboxPlatno.Size = New System.Drawing.Size(673, 368)
+        Me.pboxPlatno.TabIndex = 0
+        Me.pboxPlatno.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel1.Controls.Add(Me.tbarSnow)
+        Me.Panel1.Controls.Add(Me.tbarSize)
+        Me.Panel1.Controls.Add(Me.GroupBox1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(107, 368)
+        Me.Panel1.TabIndex = 1
+        '
+        'tbarSnow
+        '
+        Me.tbarSnow.Location = New System.Drawing.Point(56, 140)
+        Me.tbarSnow.Maximum = 100
+        Me.tbarSnow.Name = "tbarSnow"
+        Me.tbarSnow.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.tbarSnow.Size = New System.Drawing.Size(45, 222)
+        Me.tbarSnow.TabIndex = 2
+        '
+        'tbarSize
+        '
+        Me.tbarSize.Location = New System.Drawing.Point(12, 140)
+        Me.tbarSize.Maximum = 100
+        Me.tbarSize.Name = "tbarSize"
+        Me.tbarSize.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.tbarSize.Size = New System.Drawing.Size(45, 222)
+        Me.tbarSize.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.pboxSnowColor)
+        Me.GroupBox1.Controls.Add(Me.pboxSkyColor)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(7, 5)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(95, 129)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Colors"
+        '
+        'pboxSnowColor
+        '
+        Me.pboxSnowColor.BackColor = System.Drawing.Color.White
+        Me.pboxSnowColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pboxSnowColor.Location = New System.Drawing.Point(25, 86)
+        Me.pboxSnowColor.Name = "pboxSnowColor"
+        Me.pboxSnowColor.Size = New System.Drawing.Size(51, 31)
+        Me.pboxSnowColor.TabIndex = 3
+        Me.pboxSnowColor.TabStop = False
+        '
+        'pboxSkyColor
+        '
+        Me.pboxSkyColor.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.pboxSkyColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pboxSkyColor.Location = New System.Drawing.Point(25, 36)
+        Me.pboxSkyColor.Name = "pboxSkyColor"
+        Me.pboxSkyColor.Size = New System.Drawing.Size(51, 31)
+        Me.pboxSkyColor.TabIndex = 2
+        Me.pboxSkyColor.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "snow:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(26, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "sky:"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel2.Controls.Add(Me.tbarWind)
+        Me.Panel2.Controls.Add(Me.tbarPosition)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel2.Location = New System.Drawing.Point(0, 368)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(780, 64)
+        Me.Panel2.TabIndex = 2
+        '
+        'tbarWind
+        '
+        Me.tbarWind.Location = New System.Drawing.Point(7, 7)
+        Me.tbarWind.Maximum = 100
+        Me.tbarWind.Minimum = -100
+        Me.tbarWind.Name = "tbarWind"
+        Me.tbarWind.Size = New System.Drawing.Size(100, 45)
+        Me.tbarWind.TabIndex = 1
+        '
+        'tbarPosition
+        '
+        Me.tbarPosition.Location = New System.Drawing.Point(107, 7)
+        Me.tbarPosition.Maximum = 100
+        Me.tbarPosition.Name = "tbarPosition"
+        Me.tbarPosition.Size = New System.Drawing.Size(670, 45)
+        Me.tbarPosition.TabIndex = 0
+        '
+        'tmrTime
+        '
+        Me.tmrTime.Enabled = True
+        Me.tmrTime.Interval = 30
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(780, 432)
+        Me.Controls.Add(Me.pboxPlatno)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.Text = "Sněhulák"
+        CType(Me.pboxPlatno, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.tbarSnow, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tbarSize, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.pboxSnowColor, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pboxSkyColor, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        CType(Me.tbarWind, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tbarPosition, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pboxPlatno As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents pboxSnowColor As PictureBox
+    Friend WithEvents pboxSkyColor As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents tmrTime As Timer
+    Friend WithEvents tbarSize As TrackBar
+    Friend WithEvents tbarPosition As TrackBar
+    Friend WithEvents tbarWind As TrackBar
+    Friend WithEvents tbarSnow As TrackBar
+End Class
---
+++ Snowman/Form1.vb
@@ -0,0 +1,137 @@
+﻿Public Class Form1
+ Private snow As New List(Of PointF)
+ Private r As New Random
+ Private ground As Integer = 5
+
+ Private Sub pboxPlatno_Paint(sender As Object, e As PaintEventArgs) Handles pboxPlatno.Paint
+  Dim snowmanColor, groundColor, lineColor, backColor As Color
+  Dim lineThickness As Integer = 3
+  backColor = pboxSkyColor.BackColor
+  groundColor = pboxSnowColor.BackColor
+  snowmanColor = pboxSnowColor.BackColor
+  e.Graphics.FillRectangle(New SolidBrush(backColor), pboxPlatno.ClientRectangle)
+  DrawSnowman(e.Graphics, snowmanColor, lineColor, lineThickness, tbarSize.Value, 5)
+  DrawGround(e.Graphics, groundColor, lineColor, lineThickness, ground)
+
+  Dim i As Integer = 0
+  Dim size As Single
+  Do Until i >= snow.Count
+   size = r.NextDouble * 2 + 4
+   If pboxPlatno.ClientRectangle.Contains(Point.Round(snow(i))) Then
+    e.Graphics.FillEllipse(New SolidBrush(snowmanColor), snow(i).X, snow(i).Y, size, size)
+   End If
+   i += 1
+  Loop
+ End Sub
+
+ Private Sub DrawSnowman(g As Graphics, snowmanColor As Color, lineColor As Color, line As Integer, heightPercent As Integer, groundPercent As Integer)
+  Dim heightPoints As Integer = Int(pboxPlatno.Height * (heightPercent * 0.01))
+  Dim groundLevel As Integer = Int(pboxPlatno.Height - pboxPlatno.Height * (groundPercent * 0.01))
+  Dim p As New Pen(Color.Black, line)
+  Dim b As New SolidBrush(snowmanColor)
+  Dim middle As Integer = pboxPlatno.Width * tbarPosition.Value * 0.01
+  Dim bottomSphere As Integer = CInt(heightPoints * 0.45)
+  Dim midSphere As Integer = CInt(heightPoints * 0.33)
+  Dim headSphere As Integer = heightPoints - bottomSphere - midSphere
+  Dim handSphere As Integer = CInt(heightPoints * 0.15)
+  Dim coal As Integer = CInt(midSphere \ 8)
+  Dim tooth As Integer = coal \ 2
+  groundLevel = groundLevel + CInt(bottomSphere * 0.05)
+  Dim bottomSphereTop As Integer = groundLevel - bottomSphere
+  Dim midSphereTop As Integer = bottomSphereTop - CInt(midSphere * 0.8)
+  Dim headSphereTop As Integer = midSphereTop - CInt(headSphere * 0.8)
+  Dim handSphereTop As Integer = midSphereTop + handSphere \ 3
+  Dim handDistance As Integer = midSphere \ 2 - CInt(handSphere * 0.2)
+  g.FillEllipse(b, middle - bottomSphere \ 2, bottomSphereTop, bottomSphere, bottomSphere)
+  g.DrawEllipse(p, middle - bottomSphere \ 2, bottomSphereTop, bottomSphere, bottomSphere)
+  g.FillEllipse(b, middle - handDistance - handSphere, handSphereTop, handSphere, handSphere)
+  g.DrawEllipse(p, middle - handDistance - handSphere, handSphereTop, handSphere, handSphere)
+  g.FillEllipse(b, middle - midSphere \ 2, midSphereTop, midSphere, midSphere)
+  g.DrawEllipse(p, middle - midSphere \ 2, midSphereTop, midSphere, midSphere)
+  g.FillEllipse(b, middle - headSphere \ 2, headSphereTop, headSphere, headSphere)
+  g.DrawEllipse(p, middle - headSphere \ 2, headSphereTop, headSphere, headSphere)
+  g.FillEllipse(b, middle + handDistance, handSphereTop, handSphere, handSphere)
+  g.DrawEllipse(p, middle + handDistance, handSphereTop, handSphere, handSphere)
+  g.FillEllipse(Brushes.Black, middle - CInt(1.4 * coal), groundLevel - 5 * coal, coal, coal)
+  g.FillEllipse(Brushes.Black, middle - CInt(1.6 * coal), groundLevel - CInt(7 * coal), coal, coal)
+  g.FillEllipse(Brushes.Black, middle - CInt(1.2 * coal), groundLevel - 9 * coal, coal, coal)
+  g.FillEllipse(Brushes.Black, middle - CInt(1.2 * coal), bottomSphereTop - CInt(1 * coal), coal, coal)
+  g.FillEllipse(Brushes.Black, middle - CInt(1.4 * coal), bottomSphereTop - CInt(3 * coal), coal, coal)
+  g.FillEllipse(Brushes.Black, middle - CInt(0.9 * coal), bottomSphereTop - CInt(5 * coal), coal, coal)
+  g.FillEllipse(Brushes.Black, middle - CInt(1.7 * coal), midSphereTop - CInt(2.85 * coal), coal, coal)
+  g.FillEllipse(Brushes.Black, middle + CInt(0.3 * coal), midSphereTop - CInt(2.8 * coal), coal, coal)
+  g.FillEllipse(Brushes.Black, middle + CInt(tooth), midSphereTop - CInt(1.5 * tooth), tooth, tooth)
+  g.FillEllipse(Brushes.Black, middle + CInt(0 * tooth), midSphereTop - CInt(1.6 * tooth), tooth, tooth)
+  g.FillEllipse(Brushes.Black, middle - CInt(1.0 * tooth), midSphereTop - CInt(1.7 * tooth), tooth, tooth)
+  g.FillEllipse(Brushes.Black, middle - CInt(2.0 * tooth), midSphereTop - CInt(1.8 * tooth), tooth, tooth)
+  g.FillEllipse(Brushes.Black, middle - CInt(3.0 * tooth), midSphereTop - CInt(1.9 * tooth), tooth, tooth)
+ End Sub
+
+ Private Sub DrawGround(g As Graphics, groundColor As Color, lineColor As Color, line As Integer, heightPercent As Integer)
+  Dim heightPoints As Integer = Int(pboxPlatno.Height * (heightPercent * 0.01))
+  Dim p As New Pen(Color.Black, line)
+  g.FillRectangle(New SolidBrush(groundColor), 0, pboxPlatno.Height - heightPoints, pboxPlatno.Width, heightPoints)
+  g.DrawLine(p, 0, pboxPlatno.Height - heightPoints, pboxPlatno.Width, pboxPlatno.Height - heightPoints)
+ End Sub
+
+ Private Sub Form1_SizeChanged(sender As Object, e As EventArgs) Handles MyBase.SizeChanged
+  tbarPosition.Width = pboxPlatno.Width
+  tbarSize.Height = pboxPlatno.Height - 150
+  tbarSnow.Height = pboxPlatno.Height - 150
+  pboxPlatno.Refresh()
+ End Sub
+
+ Private Sub pboxSkyColor_Click(sender As Object, e As EventArgs) Handles pboxSkyColor.Click
+  ColorDialog1.Color = pboxSkyColor.BackColor
+  ColorDialog1.ShowDialog()
+  pboxSkyColor.BackColor = ColorDialog1.Color
+  pboxPlatno.Refresh()
+ End Sub
+
+ Private Sub pboxSnowColor_Click(sender As Object, e As EventArgs) Handles pboxSnowColor.Click
+  ColorDialog1.Color = pboxSnowColor.BackColor
+  ColorDialog1.ShowDialog()
+  pboxSnowColor.BackColor = ColorDialog1.Color
+  pboxPlatno.Refresh()
+ End Sub
+
+ Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+  tbarSize.Value = 75
+  tbarPosition.Value = 50
+ End Sub
+
+ Private Sub tbarSize_Scroll(sender As Object, e As EventArgs) Handles tbarSize.Scroll, tbarPosition.Scroll
+  pboxPlatno.Refresh()
+ End Sub
+
+ Private Sub tmrTime_Tick(sender As Object, e As EventArgs) Handles tmrTime.Tick
+
+
+
+  Me.Text = $"Sněhulák - {snow.Count} vloček"
+  For j = 1 To Int(tbarSnow.Value * 0.8)
+   snow.Add(New PointF(r.Next(-pboxPlatno.Width, 2 * pboxPlatno.Width), 0))
+  Next
+  Dim i As Integer = 0
+  Do Until i >= snow.Count
+   If snow(i).Y > Int(pboxPlatno.Height - pboxPlatno.Height * (ground * 0.01)) Then
+    snow.RemoveAt(i)
+   Else
+    snow(i) = New PointF(snow(i).X + r.NextDouble() * 2 - 1 + tbarWind.Value * 0.04, snow(i).Y + r.Next(1, 5))
+    i += 1
+   End If
+  Loop
+  pboxPlatno.Refresh()
+ End Sub
+
+ Private Sub pboxPlatno_Click(sender As Object, e As EventArgs) Handles pboxPlatno.Click
+  tmrTime.Enabled = Not tmrTime.Enabled
+
+ End Sub
+
+ Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+  If e.KeyCode = Keys.Space Then
+   tmrTime.Enabled = Not tmrTime.Enabled
+  End If
+ End Sub
+End Class
